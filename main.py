8d7c684d96c448f9ca7e4ada8e5489d6bc2b7742
Julian <mhhy76dvmv@privaterelay.appleid.com>
2026-04-21 20:35:21 +0800

    跨域问题

--- main.py
+++ main.py
@@ -1,7 +1,18 @@
 from fastapi import FastAPI
 from routers import news
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+# 添加 CORS 中间件
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # 允许访问的源,开发阶段可以设置为 ["*"]，生产环境需要指定具体域名
+    allow_credentials=True,  # 允许携带 Cookie
+    allow_methods=["*"],  # 允许所有请求方法
+    allow_headers=["*"],  # 允许所有请求头
+)
+
 
 @app.get("/")
 async def root():
